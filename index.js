dabcf351e1ad024f07fc3241d5d5e9b67332ddea
Josh484 <josh.m.mendoza1@gmail.com>
2024-01-19 19:47:09 +0000

    created inquirer for manager

--- index.js
+++ index.js
@@ -10,6 +10,48 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+const checkEmail = async (input) => {
+    return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(input)
+}
+const checkLetters = async (input) => {
+    return /[a-zA-Z]/.test(input)
+}
+const validateNumber = async (answer) => {
+    if (isNaN(answer)) {
+        return "please enter a number";
+      }
+      return true;
+}
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+questions = [
+    {
+    type: 'input',
+    message: 'Name?',
+    name: 'eName',
+    validate: checkLetters
+    },
+    {
+        type: 'input',
+        message: 'Employee ID',
+        name: 'id',
+        validate: validateNumber
+    },
+    {
+        type: 'input',
+        message: 'What is your E-mail?',
+        name: 'email',
+        validate: checkEmail
+    },
+    {
+        type: 'number',
+        message: 'Office Number',
+        name: 'oNumber',
+        validate: validateNumber
+    },
+]
 
+
+inquirer.prompt(questions).then(function (response) {
+
+})
